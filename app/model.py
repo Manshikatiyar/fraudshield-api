6a2e6a61c551d5a5fd51b29871f4bb4dadc7baef
Manshikatiyar <nishukatiyar97@gmail.com>
2026-06-11 14:50:30 +0530

    fix feature mismatch and deployment stability

--- app/model.py
+++ app/model.py
@@ -2,13 +2,12 @@ import joblib
 import numpy as np
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 model_path = os.path.join(BASE_DIR, "model", "xgboost_model.joblib")
 
 model = joblib.load(model_path)
 
 FEATURE_ORDER = [
-    "Time",
     "V1","V2","V3","V4","V5","V6","V7","V8","V9","V10",
     "V11","V12","V13","V14","V15","V16","V17","V18","V19","V20",
     "V21","V22","V23","V24","V25","V26","V27","V28","Amount"
@@ -17,7 +16,7 @@ FEATURE_ORDER = [
 def predict_fraud(data):
     input_data = np.array([[getattr(data, f) for f in FEATURE_ORDER]])
 
-    prob = model.predict_proba(input_data)[0][1]   # fraud probability
+    prob = model.predict_proba(input_data)[0][1]
     pred = int(prob > 0.5)
 
     return {
